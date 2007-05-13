9162b44c118d946c8e00fa1097484f7f9dc61f2a
Lucian Cojocar <noemail@rosedu.org>
2007-05-13 17:41:29 +0000

    wxWidgets (C++) apeleaza o functie dintr-o biblioteca C

---
+++ test/cspay.c
@@ -0,0 +1,18 @@
+#include <wchar.h>
+#include "cspay.h"
+
+#ifdef __cplusplus
+#	error "Aici este cod C"
+#endif
+
+float
+media(int a, int b, int c)
+{
+	return (float)(a + b + c) / 3;
+}
+
+char *
+mesaj()
+{
+	return "Ana sparge cana";
+}
