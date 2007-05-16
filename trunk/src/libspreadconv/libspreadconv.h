9d3f04fdeadbca214dda43eba0861a1940cb3d0c
Vlad Dogaru <noemail@rosedu.org>
2007-05-16 22:22:33 +0000

    Initial draft of libspreadconv.h

---
+++ trunk/src/libspreadconv/libspreadconv.h
@@ -0,0 +1,26 @@
+/*
+ * libspreadconv header
+ * First draft: 16.05.2007, Vlad
+ */
+
+/* file type constants */
+#define LSC_FILE_ODS		1 /* OPF spreadsheet */
+#define LSC_FILE_XLSX		2 /* Office Open XML spreadsheet */
+#define LSC_FILE_XLS		4 /* Classic MS Excel spreadsheet */
+
+/* generic spreadsheet data container */
+/* TODO: add metadata, minimal style data eventually */
+struct spreadconv_data {
+	/* number of rows and columns */
+	int nrows, ncols;
+	/* spreadsheet cells */
+	char ***cells;
+};
+
+/* 
+ * Converts a generic spreadconv_data structure into the associated
+ * file(s). The second parameter is a bit field which determines which
+ * formats are to be used for the conversion. The new file's name,
+ * without an extension, is returned.
+ */
+char* spreadconv_create_spreadsheet(struct spreadconv_data *, int);
