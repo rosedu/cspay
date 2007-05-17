426434cd04d15d25099b0b430b5f87373256943a
Vlad Dogaru <noemail@rosedu.org>
2007-05-17 20:47:21 +0000

    Initial draft of specification sketch

--- trunk/src/libspreadconv/libspreadconv.h
+++ trunk/src/libspreadconv/libspreadconv.h
@@ -3,7 +3,11 @@
  * First draft: 16.05.2007, Vlad
  */
 
-/* file type constants */
+/* 
+ * file type constants 
+ * Initially, only ODS will be implemented. Others will be included via
+ * external converters.
+ */
 #define LSC_FILE_ODS		1 /* OPF spreadsheet */
 #define LSC_FILE_XLSX		2 /* Office Open XML spreadsheet */
 #define LSC_FILE_XLS		4 /* Classic MS Excel spreadsheet */
