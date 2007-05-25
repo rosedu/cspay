ab5cb37bf90426c5f0e8c1036e219c55bcfef08f
Alex Eftimie <noemail@rosedu.org>
2007-05-25 16:43:19 +0000

    Adaugat parsing cspay.xml - prima faza. Utilizand DOM, poate fi facut si altfel...

--- trunk/src/iw/index.php
+++ trunk/src/iw/index.php
@@ -4,17 +4,69 @@
  *	Citeste cspay.xml si afiseaza formularul.
  *
  */
- 
-# incarc continutul html
+define('CSPAY_XML_URI','http://anaconda.cs.pub.ro/~cspay/cspay.xml'); //discutabila alegere...
+
+# metoda recursiva de transformat XML DOM in dictionar
+function dom2array( $node ) {
+	$result = array();
+	if($node->nodeType == XML_TEXT_NODE) {
+		$result = $node->nodeValue;
+	}
+	else {
+		if( $node->hasAttributes() ) {
+		        $attributes = $node->attributes;
+		        if(!is_null($attributes)) 
+			        foreach ($attributes as $index=>$attr) 
+		        	   $result[$attr->name] = $attr->value;
+		}
+		if($node->hasChildNodes()){
+		        $children = $node->childNodes;
+		        for($i=0;$i<$children->length;$i++) {
+		        	$child = $children->item($i);
+		        	if($child->nodeName != '#text')
+		        		if(!isset($result[$child->nodeName]))
+		              			$result[$child->nodeName] = dom2array($child);
+		              		else {
+		              			$aux = $result[$child->nodeName];
+		              			$result[$child->nodeName] = array( $aux );
+		              			$result[$child->nodeName][] = dom2array($child);
+		              		}
+		        }
+		}
+	}
+	return $result;
+}
+
+# incarc continutul html TODO it somehow else
 $template = implode("",@file('model-form.html'));
 
+# incarc cspay.xml
+$doc = new DOMDocument();
+$doc->load(CSPAY_XML_URI);
+$cspay = dom2array( $doc );
+
+# TODO: verificari de corectitudine and so
+$universitate = $cspay['cspay-config']['universitate'];
+
 # pregatesc campurile care se inlocuiesc: facultati, catedre
-# TODO: read from xml
-$facultati_info = array('ACS'=>'Automatica si Calculatoare','Electro'=>'Electrocutari');
+if(!isset($universitate['facultati']['facultate']['nume']))
+	$universitate['facultati'] = $universitate['facultati']['facultate'];
+	
+foreach( $universitate['facultati'] as $facultate) {
+	$facultati_info[$facultate['nume_scurt']] = $facultate['nume'];
+	# adaug si catedrele acestei facultati
+	if( !isset($facultate['catedra']['nume']) ) {
+		foreach( $facultate['catedra'] as $catedra)
+			$catedre_fac[$facultate['nume_scurt']][] = $catedra['nume'];
+	}
+	else
+		$catedre_fac[$facultate['nume_scurt']][] = $facultate['catedra']['nume'];
+}
+
+# am obtinut facultati_info si catedre_fac; transform in HTML:
 foreach( $facultati_info as $nume => $descriere )
 	$facultati .= "<option value=\"$nume\">$descriere</option>\n";
-	
-$catedre_fac = array('ACS' => array('USO', 'SDA'), 'Electro'=>array('Fizica','Bla blauri'));
+
 foreach( $catedre_fac as $nume => $catedrele) {
 	$catedre .= "<select name=\"".$nume."_catedre\" id=\"".$nume."_catedre\" class=\"hidden\">\n";
 	foreach( $catedrele as $c )
