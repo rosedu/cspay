7cd25a6e82d5469ea75ed4f2ee800779180dc84c
Vlad Dogaru <noemail@rosedu.org>
2007-06-18 11:53:04 +0000

    Specification  sketch done Data structures done Most library code written, but untested No makefile, this almost certainly fails to compile Probably in need of a better code organization Doxygen comments in place

---
+++ trunk/src/libspreadconv/spreadconv.c
@@ -0,0 +1,471 @@
+/*
+ * libspreadconv main file
+ * First draft: 15.06.2007, Vlad Dogaru
+ */
+
+/**
+ * \file spreadconv.c
+ * \brief libspreadconv main implementation file
+ * \author Vlad Dogaru
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include "spreadconv.h"
+
+/** Global external error code */
+extern int errno;
+
+/**
+ * XML namespace declaration. An XML namespace--URL pair.
+ * \todo This type should be hidden. Apparently static has no effect in
+ * this context, so I'm all out of suggestions for now. -Vlad
+ */
+struct spreadconv_xml_namespace {
+	/** namespace name */
+	char *name;
+	/** namespace URL */
+	char *url;
+}
+
+/** Number of existing XML namespaces */
+static const int xml_namespace_count = 7;
+
+/**
+ * Existing XML namespaces. These are all written in the root element of
+ * all files in an ODS package.
+ */
+static const struct spreadconv_xml_namespace xml_namespaces[] = {
+	{ "office", "urn:oasis:name:tc:opendocument:xmlns:office:1.0" },
+	{ "table", "urn:oasis:name:tc:opendocument:xmlns:table:1.0" },
+	{ "text", "urn:oasis:name:tc:opendocument:xmlns:text:1.0" },
+	{ "meta", "urn:oasis:name:tc:opendocument:xmlns:meta:1.0" },
+	{ "manifest", "urn:oasis:name:tc:opendocument:xmlns:manifest:1.0" },
+	{ "fo", "urn:oasis:name:tc:opendocument:xmlns:xsl-fo-compatible:1.0" },
+	{ "dc", "http://purl.org/dc/elements/1.1" }
+}
+
+char *
+spreadconv_convert_column_number(int n)
+{
+	int max_value, string_length;
+	int i;
+	char *text;
+	
+	if (n <= 0)
+		return 0;
+		
+	for (max_value=26, string_length=1; 
+			n > max_value;
+			max_value *= 27, string_length++)
+		;
+	
+	text = malloc(string_legth+1);
+	if (text == 0) 
+		return 0;
+
+	text[length] = 0;
+	for (; string_legth>=1; string_length--) {
+		text[string_legth-1] = n % 26;
+		n /= 26;
+	}
+
+	return text;
+}
+
+/**
+ * Prints all the needed namespaces to a stream. The namespaces are
+ * formatted accordingly and should be attributes to the root element.
+ * \param f The stream to write to
+ */
+static void
+print_namespaces(FILE *f)
+{
+	int i;
+
+	for (i=0; i<xml_namespace_count; i++)
+		fprintf(f, "xmlns:%s=\"%s\" ", 
+				xml_namespaces[i].name,
+				xml_namespaces[i].url);
+}
+
+/**
+ * Creates the manifest file for a package. The manifest file is
+ * basically always the same for standard, minimal, unencrypted packages
+ * such as the ones we are creating. Thus, there is no need to pass it
+ * a \a spreadconv_data structure.
+ * \param data Not currently used, but here for further expansion. We
+ * thus avoid changing function prototypes later on.
+ * \remarks This function assumes that the current directory is that
+ * corresponding to a package and that the user is permitted to create
+ * new files and directories. It creates the directory META-INF, with
+ * the file manifest.xml in it.
+ * \return 0 on success, -1 on error, and errno is set accordingly by
+ * the last function call
+ */
+static int
+create_manifest_file(struct spreadconv_data *data)
+{
+	FILE *f;
+	int i;
+	const char *filenames[] = { "content", "meta", "settings", "style" };
+	const int nfiles = 4;
+
+	/* attempt to create directory */
+	if (mkdir("META-INF", S_IFDIR | S_IRUSR | S_IWUSR) == -1)
+		return -1;
+
+	f = fopen("META-INF/manifest.xml", "w");
+	if (f == 0)
+		return -1;
+
+	fprintf(f, "<?xml version=\"1.0\"?>\n");
+	fprintf(f, "<manifest:manifest ");
+	print_namespaces(f);
+	fprintf(f, ">\n");
+	fprintf(f, "<manifest:file-entry "
+			"manifest:media-type=\"application/vnd.oasis.opendocument.spreadsheet\" "
+			"manifest:full-path=\"/\" />\n");
+	for (i=0; i<nfiles; i++)
+		fprintf(f, "<manifest:file-entry "
+				"manifest:media-type=\"text/xml\" "
+				"manifest:full-path=\"%s.xml\" />\n",
+				filenames[i]);
+	fprintf(f, "</manifest:manifest>\n");
+	fclose(f);
+	return 0;
+}
+
+/**
+ * Creates the mimetype file. This contains a single line, specifying
+ * the mimetype of the file. It must not contain an end-of-line
+ * character.
+ * \return 0 on success, -1 on error, and errno set accordingly.
+ */
+static int
+create_mimetype_file(void)
+{
+	FILE *f;
+
+	f = fopen("mimetype", "w");
+	if (f == 0)
+		return -1;
+
+	fprintf(f, "application/vnd.oasis.opendocument.spreadsheet");
+
+	fclose(f);
+	return 0;
+}
+
+/**
+ * Creates the settings file. This is basically an empty file that
+ * applications should fill with their specific settings. It is present
+ * in the archive for standards conformance.
+ * \param data Not currently used
+ * \return 0 on success, -1 on error
+ */
+static int
+create_settings_file(struct spreadconv_data *data)
+{
+	FILE *f;
+
+	f = fopen("settings.xml", "w");
+	if (f == 0)
+		return -1;
+
+	fprintf(f, "<?xml version=\"1.0\"?>\n");
+	fprintf(f, "<office:document-settings ");
+	print_namespaces(f);
+	fprintf(f, "/>\n");
+
+	fclose(f);
+	return 0;
+}
+
+/**
+ * Creates the style file. This is also empty -- style information goes
+ * in content.xml, because, for some unknown reason, including style
+ * information in this file crashes both ods readers I have tried.
+ * \param data Not currently used
+ * \return 0 on success, -1 on error
+ */
+static int
+create_style_file(struct spreadconv_data *data)
+{
+	FILE *f;
+
+	f = fopen("styles.xml", "w");
+	if (f == 0)
+		return -1;
+
+	fprintf(f, "<?xml version=\"1.0\"?>\n");
+	fprintf(f, "<office:document-styles ");
+	print_namespaces(f);
+	fprintf(f, "/>\n");
+
+	fclose(f);
+	return 0;
+}
+
+/**
+ * Creates the meta file. This contains information about the file
+ * generator, namely libspreadconv.
+ * \param data Not currently used
+ * \return 0 on success, -1 on error
+ */
+static int
+create_meta_file(struct spreadconv_data *data)
+{
+	FILE *f;
+
+	f = fopen("meta.xml", "w");
+	if (f == 0)
+		return -1;
+
+	fprintf(f, "<?xml version=\"1.0\"?>\n");
+	fprintf(f, "<office:document-meta ");
+	print_namespaces(f);
+	fprintf(f, ">\n");
+	fprintf(f, "<office:meta>\n");
+	fprintf(f, "<meta:generator>%s %s</meta:generator>\n",
+			LSC_NAME, LSC_VERSION);
+	fprintf(f, "</office:meta>\n");
+
+	fclose(f);
+	return 0;
+}
+
+/**
+ * Prints a row or column style to a file.
+ * \param style Pointer to the style to print
+ * \param f The stream to print to
+ */
+static void
+print_rc_style(struct spreadconv_rc_style *style, FILE *f)
+{
+	fprintf(f, "<style:style style:name=\"\%s\" ", style->name);
+	if (style->type == LSC_STYLE_ROW) {
+		fprintf(f, "style:family=\"table-row\">\n");
+		fprintf(f, "<style:table-row-properties style:row-height=\"%s\" />\n",
+				style->size);
+	} else {
+		/* assuming LSC_STYLE_COLUMN */
+		fprintf(f, "style:family=\"table-column\">\n");
+		fprintf(f, "<style:table-column-properties style:column-width=\"%s\" />\n",
+				style->size);
+	}
+	fprintf(f, "</style:style>\n");
+}
+
+/**
+ * Prints a cell style to a file.
+ * \param style Pointer to the style to print
+ * \param f The stream to print to
+ */
+static void
+print_cell_style(struct spreadconv_cell_style *style, FILE *f)
+{
+	fprintf(f, "<style:style style:name=\"%s\" ", style->name);
+	fprintf(f, "style:family=\"table-cell\">\n");
+	fprintf(f, "<style:table-cell-properties ");
+	if (style->valign != 0) 
+		fprintf(f, "style:vertical-align=\"%s\" ", style->valign);
+	if (style->border != 0)
+		fprintf(f, "fo:border=\"%s\" ", style->border);
+	if (style->border_top != 0)
+		fprintf(f, "fo:border-top=\"%s\" ", style->border_top);
+	if (style->border_bottom != 0)
+		fprintf(f, "fo:border-bottom=\"%s\" ", style->border_bottom);
+	if (style->border_left != 0)
+		fprintf(f, "fo:border-left=\"%s\" ", style->border_left);
+	if (style->border_right != 0)
+		fprintf(f, "fo:border-right=\"%s\" ", style->border_right);
+	fprintf(f, "/>\n");
+	fprintf(f, "</style:style>\n");
+}
+
+/**
+ * Prints a table cell to a file.
+ * \remarks Currently only float, string and formula are supported.
+ * \param cell Pointer to the cell to print
+ * \param f The spream to print to
+ */
+static void
+print_table_cell(struct spreadconv_cell *cell, FILE *f)
+{
+	/* avoid printing rubbish if text is NULL */
+	if (cell->text == 0)
+		cell->text = strdup("");
+
+	fprintf(f, "<table:table-cell ");
+	if (cell->style != 0) 
+		fprintf(f, "table:style-name=\"%s\" ", cell->style->name);
+
+	if ((cell->value_type == NULL) || 
+			(strncmp(cell->value_type, "string", strlen("string")) == 0)) {
+		fprintf(f, ">\n");
+		fprintf(f, "<text:p>%s</text:p>\n", cell->text?cell->text:"");
+		fprintf(f, "</table:table-cell>\n");
+		return;
+	}
+
+	if (strncmp(cell->value_type, "formula", strlen("formula")) == 0) {
+		fprintf(f, "table:formula=\"%s\" />\n", cell->text);
+		return;
+	}
+
+	if (strncmp(cell->value_type, "float", strlen("float")) == 0) {
+		fprintf(f, "office:value-type=\"float\" ");
+		fprintf(f, "office:value=\"%s\">\n", cell->value);
+		fprintf(f, "<text:p>%s</text:p>\n", cell->value);
+		fprintf(f, "</table:table-cell>\n");
+		return;
+	}
+
+	/* If we got here, then the value_type is not supported */
+	fprintf(stderr, "Cell value type not supported: %s\n", cell->value_type);
+	assert(0);
+}
+
+/**
+ * Creates the content file. This is where most of the text (and the
+ * code) actually resides. It is also the only file creation routine so
+ * far that actually uses the data it is supplied with.
+ * \param data A \a spreadconv_data structure from which to draw
+ * information
+ * \return 0 on success, -1 on error
+ */
+static int
+create_content_file(struct spreadconv_data *data)
+{
+	FILE *f;
+	int i, j;
+
+	f = fopen("content.xml", "w");
+	if (f == 0)
+		return -1;
+
+	fprintf(f, "<?xml version=\"1.0\"?>\n");
+	fprintf(f, "<office:document-content ");
+	print_namespaces(f);
+	fprintf(f, ">\n");
+
+	/* print style information */
+	fprintf(f, "<office:automatic-styles>\n");
+	for (i=0; i<data->n_unique_rc_styles; i++)
+		print_rc_style(data->unique_rc_styles[i], f);
+	for (i=0; i<data->n_unique_cell_styles; i++)
+		print_cell_style(data->unique_cell_styles[i], f);
+	fprintf(f, "</office:automatic-styles>\n");
+
+	/* print the actual spreadsheet */
+	fprintf(f, "<office:body>\n");
+	fprintf(f, "<office:spreadsheet>\n");
+	
+	/* avoid printing garbage if data->name is NULL */
+	if (data->name == 0)
+		data->name = strdup("Sheet 1");
+	
+	fprintf(f, "<table:table table:name=\"%s\">\n", data->name);
+	
+	/* print the columns */
+	for (i=0; i<n_cols; i++) {
+		fprintf(f, "<table:table-column ");
+		if (data->col_styles[i] != 0)
+			fprintf(f, "table:style-name=\"%s\" ", 
+					data->col_styles[i]->name);
+		fprintf(f, "/>\n");
+	}
+
+	/* print the rows, each with the associated cells */
+	for (i=0; i<n_rows; i++) {
+		fprintf(f, "<table:table-row ");
+		if (data->row_styles[i] != 0)
+			fprintf(f, "table:style-name=\"%s\" ",
+					data->row_styles[i]->name);
+		fprintf(f, ">\n");
+		
+		for (j=0; j<n_cols; j++)
+			print_table_cell(&data->cells[i][j], f);
+		fprintf(f, "</table:table-row>\n");
+	}
+
+	fprintf(f, "</table:table>\n");
+	fprintf(f, "</office:spreadsheet>\n");
+	fprintf(f, "</office:body>\n");
+	fprintf(f, "</office:document-content>\n");
+	fclose(f);
+	return 0;
+}
+
+
+
+/**
+ * Converts a generic \a spreadconv_data structure into the associated
+ * file(s).
+ * \param data The data to covert to a spreadsheet
+ * \param file_types A bit field indicating which file types to create
+ * \remarks Only ods format is currently supported
+ * \returns The created file's name, without extension
+ */
+char * 
+spreadsheet_conv_create_spreadsheet(struct spreadconv_data *data, int file_types)
+{
+	FILE *f; /* the file which will be written */
+	int fd; /* file descriptor returned by mkstemp */
+	int i,j;
+	char *dirname;
+	char *prev_dir;
+	char *buffer, *buffer2;
+
+	/* Only ODS files are currently supported. */
+	assert(file_types == LSC_FILE_ODS);
+
+	prev_dir = getcwd(prev_dir, 0);
+	if (chdir(spreadconv_dir_name) == -1) {
+		free(spreadconv_dir_name);
+		spreadconv_dir_name = strdup("/tmp");
+		chdir(spreadconv_dir_name);
+	}
+
+	dirname = strdup("libspreadconvXXXXXX");
+	if (mkdtemp(dirname) == 0)
+		return 0;
+
+	chdir(dirname);
+
+	/* create the files for the package*/
+	create_manifest_file(data);
+	create_mimetype_file();
+	create_settings_file(data);
+	create_styles_file(data);
+	create_meta_file(data);
+	create_content_file(data);
+
+	/* package the files according to the standard */
+	buffer = malloc(100);
+	snprintf(buffer, 100, "zip %s.ods mimetype META-INF/manifest.xml "
+			"content.xml meta.xml styles.xml settings.xml -n mimetype", dirname);
+	system(buffer);
+
+	/* 
+	 * Move created file a level up and delete the temporary
+	 * directory.
+	 */
+	buffer2 = malloc(100);
+	snprintf(buffer, 100, "%s.ods", dirname);
+	snprintf(buffer2, 100, "../%s", buffer);
+	rename(buffer, buffer2);
+
+	chdir("..");
+	remove(dirname);
+	
+	chdir(prev_dir);
+	return dirname;
+}
--- trunk/src/libspreadconv/spreadconv.h
+++ trunk/src/libspreadconv/spreadconv.h
@@ -1,52 +1,172 @@
 /*
  * libspreadconv header
- * First draft: 16.05.2007, Vlad
+ * First draft: 16.05.2007, Vlad Dogaru
+ */
+
+/**
+ * \file spreadconv.h
+ * libspreadconv header file
+ * \author Vlad Dogaru
  */
 
 #ifndef _SPREADCONV_H_
 #define _SPREADCONV_H_
 
-/* 
- * file type constants 
- * Initially, only ODS will be implemented. Others will be included via
- * external converters.
+/**
+ * ODF spreadsheet
+ * \remarks Initially, only ODS will be implemented. Others will be 
+ * included via external converters.
  */
-#define LSC_FILE_ODS		1 /* OPF spreadsheet */
-#define LSC_FILE_XLSX		2 /* Office Open XML spreadsheet */
-#define LSC_FILE_XLS		4 /* Classic MS Excel spreadsheet */
+#define LSC_FILE_ODS		1
+#define LSC_FILE_XLSX		2 /**< Office Open XML spreadsheet */
+#define LSC_FILE_XLS		4 /**< Classic MS Excel spreadsheet */
 
-/* generic spreadsheet data container */
-/* TODO: add metadata, minimal style data eventually */
-struct spreadconv_data {
-	/* number of rows and columns */
-	int nrows, ncols;
-	/* spreadsheet cells */
-	char ***cells;
+#define LSC_STYLE_ROW		1 /**< specifies a row style */
+#define LSC_STYLE_COL		2 /**< specifies a column style */
+
+/** Application name */
+#define LSC_NAME 		"libspreadconv"
+/** Application version */
+#define LSC_VERSION		"0.1"
+
+/**
+ * \struct spreadconv_rc_style
+ * style information for a spreadsheet row or column
+ */
+struct spreadconv_rc_style {
+	/** style name */
+	char *name;
+	/** stores whether the style is for a row or for a column */
+	char type;
+	/** 
+	 * size information -- row height or column width. This is a
+	 * char* because the user can specify a measure unit right after
+	 * the value, such as "5cm".
+	 */
+	char *size;
 };
 
-/*
- * RD: I would like this better (wit
+/**
+ * \struct spreadconv_cell_style
+ * style information for a spreadsheet cell
  */
-#if 0
+struct spreadconv_cell_style {
+	/** style name */
+	char *name;
+	/** vertical alignment
+	 * \remarks Possible values include: top, middle, bottom,
+	 * automatic */
+	char *valign;
+	/** 
+	 * general border specifier
+	 * \remarks If this is set, all other border specificators are
+	 * ignored and the cell is identically bordered on all four
+	 * sides.
+	 *
+	 * Borders are specified as strings with three space-separated
+	 * entities as follows:
+	 *  - width (e.g. 0.05in, might also be possible to use cm)
+	 *  - line style (solid should be the only one needed)
+	 *  - color, in HTML code (#000000 for black)
+	 */
+	char *border;
+	/** top border specifier */
+	char *border_top;
+	/** bottom border specifier */
+	char *border_bottom;
+	/** left border specifier */
+	char *border_left;
+	/** right border specifier */
+	char *border_right;
+};
+
+/**
+ * \struct spreadconv_cell
+ * spreadsheet cell structure
+ * \remarks suggested by Razvan. Thank you :-)
+ */
 struct spreadconv_cell {
-	char *info;
-	/* metadata/style/etc. could go here */
+	/** 
+	 * The value type of the cell.
+	 * \remarks Numbers should get "float", regardless if they are
+	 * float or integer. Other possible values are date, percentage,
+	 * currency, time, boolean or string, although they might not
+	 * all be implemented. String is assumed if this is NULL. A
+	 * special value (not in the standard) is ``formula'', which
+	 * will cause libspreadconv to generate code for a cell
+	 * containing the formula specified in \a text.
+	 */
+	char *value_type;
+	/**
+	 * the text contained in the cell.
+	 * \remarks Although \a value_type may be numerical, its value is
+	 * stored as a string.
+	 */
+	char *text;
+	/**
+	 * the cell's style
+	 *
+	 * This references a \a spreadconv_cell_style contained within
+	 * the same \a spreadconv_data structure.
+	 */
+	struct spreadconv_cell_style *style;
 };
 
+/**
+ * \struct spreadconv_data
+ * generic spreadsheet data container
+ * \remarks for the moment, only single-sheet spreadsheets are supported
+ */
 struct spreadconv_data {
-	/* number of rows and columns */
-	int nrows, ncols;
-	/* spreadsheet cells */
+	/** name of the sheet */
+	char *name;
+	/** number of rows */
+	int n_rows;
+	/** number of columns */
+	int n_cols;
+
+	/** number of unique row/column styles */
+	int n_unique_rc_styles;
+	/** unique row/column style specification */
+	struct spreadconv_rc_style *unique_rc_styles;
+
+	/** 
+	 * row styles. Contains a pointer to a unique row style for each
+	 * row. So the length is always \a nrows.
+	 */
+	struct spreadconv_rc_style **row_styles;
+	/* column styles. Contains a pointer to a unique row/column style
+	 * for each column.  So the length is always \a ncols.
+	 */
+	struct spreadconv_rc_style **col_styles;
+
+	/** number of unique cell styles */
+	int n_unique_cell_styles;
+	/** unique cell styles */
+	struct spreadconv_cell_styles *unique_cell_styles;
+
+	/**
+	 * spreadsheet cells. A \a nrows by \a ncols matrix of cells.
+	 */
 	struct spreadconv_cell **cells;
 };
-#endif
 
-/* 
- * Converts a generic spreadconv_data structure into the associated
- * file(s). The second parameter is a bit field which determines which
- * formats are to be used for the conversion. The new file's name,
- * without an extension, is returned.
+/**
+ * Name of the directory in which to store generated files.
+ * \remarks If it is NULL or not found, "/tmp" is used
  */
-char* spreadconv_create_spreadsheet(struct spreadconv_data *, int);
+char *spreadconv_dir_name;
+
+/**
+ * Converts a generic \a spreadconv_data structure into the associated
+ * file(s).
+ */
+char * spreadconv_create_spreadsheet(struct spreadconv_data *, int);
+
+/**
+ * Converts a number to its representation in letters; 1 is A, 2 is
+ * B, 27 is AA, 28 is AB etc.
+ */
+char * spreadconv_convert_column_number(int);
 
 #endif /* _SPREADCONV_H_ */
