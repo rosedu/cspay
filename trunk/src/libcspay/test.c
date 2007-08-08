17c0666688b66536af075e5645778d7184ee7122
Lucian Cojocar <noemail@rosedu.org>
2007-08-08 06:49:45 +0000

    modificat putin testul

--- trunk/src/libcspay/test.c
+++ trunk/src/libcspay/test.c
@@ -7,14 +7,20 @@
 #include "cspay.h"
 
 int
-main()
+main(int argc, char **argv)
 {
 	struct cspay_config *cfg;
 	struct cspay_file_list *fl;
-	cfg = cspay_get_config(NULL);
-	fl = cspay_convert_options(cfg, "personal.ini");
+	if (argc != 3) {
+		printf("%s xml_file ini_file\n", argv[0]);
+		return -1;
+	}
+	cfg = cspay_get_config(argv[1]);
+	fl = cspay_convert_options(cfg, argv[2]);
 	cspay_free_config(cfg);
-	printf("Creat: %s\n", fl->names[0]);
+	int i;
+	for (i = 0; i < fl->nr; ++ i)
+		printf("%s\n", fl->names[i]);
 	cspay_free_file_list(fl);
 	return 0;
 }
