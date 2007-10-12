d3f4f79ba97c8d5636df0f4114fa083f5edb80dc
Lucian Cojocar <noemail@rosedu.org>
2007-10-12 10:27:58 +0000

    svn test

--- trunk/src/iw/generate.php
+++ trunk/src/iw/generate.php
@@ -98,7 +98,6 @@ if( !empty($lista_fisiere) ) {
 	echo "<h1>Fişiere tabel de date obţinute:</h1><ul>";
 	
 	for($i = 0; $i<count($lista_fisiere); $i++) {
-		# CL: am modificat pe aici
 		$fn =  str_replace(array("/tmp/"), "", $lista_fisiere[$i]);
 		echo "<li><a href=\"download.php?f=$fn\">$fn</a></li>\n";
 	}
