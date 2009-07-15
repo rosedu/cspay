5f48cf873a5fa867202ef3ee2c3adfecc985bb22
init <noemail@rosedu.org>
2009-07-15 17:52:04 +0000

    update mysql-cspay-ore-fill

--- branches/db/mysql-cspay-fill.sql
+++ branches/db/mysql-cspay-fill.sql
@@ -2,7 +2,8 @@
 -- Dumping data for table `universitati`
 -- 
 
-INSERT INTO `universitati` (`univ_id`, `nume`, `data_start`, `data_stop`) VALUES (1, 'Universitatea Politehnica Bucuresti', '2009-02-23', '2008-06-05');
+INSERT INTO `universitati` (`nume`, `data_start`, `data_stop`) VALUES
+('Universitatea Politehnica Bucuresti', '2009-02-23', '2008-06-05');
 
 -- 
 -- Dumping data for table `vacante`
@@ -12,58 +13,137 @@ INSERT INTO `universitati` (`univ_id`, `nume`, `data_start`, `data_stop`) VALUES
 -- Dumping data for table `vacante`
 -- 
 
-INSERT INTO `vacante` (`vac_id`, `link_univ`, `data_start`, `data_stop`) VALUES 
-(8, 1, '2008-04-28', '2008-05-04');
+INSERT INTO `vacante` (`link_univ`, `data_start`, `data_stop`) VALUES 
+(1, '2008-04-20', '2008-04-26');
 
 --
 -- Dumping data for table `facultati`
 --
 
-INSERT INTO `facultati` (`fac_id`, `link_univ`, `nume`, `nume_scurt`, `decan`) VALUES
-(1, 1, 'Facultatea de Automatica si Calculatoare', 'A & C', 'Dumitru Popescu');
+INSERT INTO `facultati` (`link_univ`, `nume`, `nume_scurt`, `decan`) VALUES
+(1, 'Facultatea de Automatica si Calculatoare', 'A & C', 'Dumitru Popescu');
 
 -- 
 -- Dumping data for table `catedre`
 -- 
 
-INSERT INTO `catedre` (`cat_id`, `link_fac`, `nume`, `sef`) VALUES 
-(1, 1, 'Automatica si Informatica Industriala', 'Radu Dobrescu'),
-(2, 1, 'Automatica si Ingineria Sistemelor', 'Ioan Dumitrache'),
-(3, 1, 'Calculatoare', 'Valentin Cristea');
+INSERT INTO `catedre` (`link_fac`, `nume`, `sef`) VALUES 
+(1, 'Automatica si Informatica Industriala', 'Radu Dobrescu'),
+(1, 'Automatica si Ingineria Sistemelor', 'Ioan Dumitrache'),
+(1, 'Calculatoare', 'Valentin Cristea');
 
 -- 
 -- Dumping data for table `discipline`
+--
 
-INSERT INTO `discipline` (`disc_id`, `link_cat`, `nume`, `nume_scurt`) VALUES
-(1, 3, 'Utilizarea Sistemelor de Operare', 'USO'),
-(2, 3, 'Programarea Calculatoarelor', 'Prog'),
-(3, 3, 'Introducere in Informatica', 'II'),
-(4, 3, 'Programare Logica', 'PL'),
-(5, 3, 'Structuri de Date', 'SD'),
-(6, 3, 'Metode Numerice', 'MN'),
-(7, 3, 'Programarea in Limbaj de Asamblare', 'PLAS'),
-(8, 3, 'Analiza Algoritmilor', 'AA'),
-(9, 3, 'Elemente de Electronica Analogica', 'EEA'),
-(10, 3, 'Programarea Orientata Obiect', 'POO'),
-(11, 3, 'Protocoale de Comunicatii', 'PC'),
-(12, 3, 'Proiectarea Algoritmilor', 'PA'),
-(13, 3, 'Paradigme de Programare', 'PP'),
-(14, 3, 'Electronica Digitala', 'ED'),
-(15, 3, 'Calculatoare Numerice 1', 'CN1');
+INSERT INTO `discipline` (`link_cat`, `nume`, `nume_scurt`) VALUES
+(3, 'Utilizarea Sistemelor de Operare', 'USO'),
+(3, 'Programarea Calculatoarelor', 'Prog'),
+(3, 'Introducere in Informatica', 'II'),
+(3, 'Programare Logica', 'PL'),
+(3, 'Structuri de Date', 'SD'),
+(3, 'Metode Numerice', 'MN'),
+(3, 'Programarea in Limbaj de Asamblare', 'PLAS'),
+(3, 'Analiza Algoritmilor', 'AA'),
+(3, 'Elemente de Electronica Analogica', 'EEA'),
+(3, 'Programarea Orientata Obiect', 'POO'),
+(3, 'Protocoale de Comunicatii', 'PC'),
+(3, 'Proiectarea Algoritmilor', 'PA'),
+(3, 'Paradigme de Programare', 'PP'),
+(3, 'Electronica Digitala', 'ED'),
+(3, 'Calculatoare Numerice 1', 'CN1'),
+(3, 'Algoritmi de Prelucrare Distribuita', 'APD'),
+(3, 'Retele Locale', 'RL'),
+(3, 'Calculatoare Numerice 2', 'CN2'),
+(3, 'Limbaje Formale si Automate', 'LFA'),
+(3, 'Elemente de Grafica pe Calculator', 'EGC'),
+(3, 'Sisteme de Operare', 'SO'),
+(3, 'Arhitectura Sistemelor de Calcul', 'ASC'),
+(3, 'Proiectarea cu Microprocesoare', 'PM'),
+(3, 'Baze de Date', 'BD'),
+(3, 'Ingineria Programarii', 'IP'),
+(3, 'Ingineria Calculatoarelor', 'IC');
 
-INSERT INTO `titulari` (`tit_id`, `link_disc`, `nume`, `an`, `serie`) VALUES
-(1, 1, 'Razvan Rughinis', 1, 'CA'),
-(2, 1, 'Alex Herisanu', 1, 'CB'),
-(3, 1, 'Razvan Deaconescu', 1, 'CC'),
-(4, 2, 'Valeriu Iorga', 1, 'CA'),
-(5, 2, 'Eugenia Kalisz', 1, 'CB'),
-(6, 2, 'Carmen Odubasteanu', 1, 'CC'),
-(7, 3, 'Francisc Iacob', 1, 'CA'),
-(8, 3, 'Francisc Iacob', 1, 'CB'),
-(9, 3, 'Francisc Iacob', 1, 'CC'),
-(10, 4, 'Serban Petrescu', 1, 'CA'),
-(11, 4, 'Mariana Mocanu', 1, 'CB'),
-(12, 4, 'Ioan Bucur', 1, 'CC'),
-(13, 5, 'Valeriu Iorga', 1, 'CA'),
-(14, 5, 'Eugenia Kalisz', 1, 'CB'),
-(15, 5, 'Florian Moraru', 1, 'CC');
+-- 
+-- Dumping data for table `titulari`
+--
+
+INSERT INTO `titulari` (`link_disc`, `nume`, `an`, `serie`) VALUES
+(1, 'Razvan Rughinis', 1, 'CA'),
+(1, 'Alex Herisanu', 1, 'CB'),
+(1, 'Razvan Deaconescu', 1, 'CC'),
+(2, 'Valeriu Iorga', 1, 'CA'),
+(2, 'Eugenia Kalisz', 1, 'CB'),
+(2, 'Carmen Odubasteanu', 1, 'CC'),
+(3, 'Francisc Iacob', 1, 'CA'),
+(3, 'Francisc Iacob', 1, 'CB'),
+(3, 'Francisc Iacob', 1, 'CC'),
+(4, 'Serban Petrescu', 1, 'CA'),
+(4, 'Mariana Mocanu', 1, 'CB'),
+(4, 'Ioan Bucur', 1, 'CC'),
+(5, 'Valeriu Iorga', 1, 'CA'),
+(5, 'Eugenia Kalisz', 1, 'CB'),
+(5, 'Florian Moraru', 1, 'CC'),
+(6, 'Valeriu Iorga', 1, 'CA'),
+(6, 'Valeriu Iorga', 1, 'CB'),
+(6, 'Florin Pop', 1, 'CC'),
+(7, 'Vasile Lungu', 2, 'CA'),
+(7, 'Vasile Lungu', 2, 'CB'),
+(7, 'Vasile Lungu', 2, 'CC'),
+(8, 'Cristian Giumale', 2, 'CA'),
+(8, 'Stefan Trausan', 2, 'CB'),
+(8, 'Andrei Mogos', 2, 'CC'),
+(9, 'Nicolae Cupcea', 2, 'CA'),
+(9, 'Nicolae Cupcea', 2, 'CB'),
+(9, 'Nicolae Cupcea', 2, 'CC'),
+(10, 'Florian Moraru', 2, 'CA'),
+(10, 'Lorina Negreanu', 2, 'CB'),
+(10, 'Carmen Odubasteanu', 2, 'CC'),
+(11, 'Valentin Cristea', 2, 'CA'),
+(11, 'Gavril Godza', 2, 'CB'),
+(11, 'Florin Pop', 2, 'CC'),
+(12, 'Vlad Posea', 2, 'CA'),
+(12, 'Stefan Trausan', 2, 'CB'),
+(12, 'Costin Chiru', 2, 'CC'),
+(13, 'Cristian Giumale', 2, 'CA'),
+(13, 'Mihaela Balint', 2, 'CB'),
+(13, 'Cristian Giumale', 2, 'CC'),
+(14, 'Nicolae Cupcea', 2, 'CA'),
+(14, 'Nicolae Cupcea', 2, 'CB'),
+(14, 'Nicolae Cupcea', 2, 'CC'),
+(15, 'Adrian Petrescu', 2, 'CA'),
+(15, 'Decebal Popescu', 2, 'CB'),
+(15, 'Nirvana Popescu', 2, 'CC'),
+(16, 'Valentin Cristea', 3, 'CA'),
+(16, 'Alexandru Costan', 3, 'CB'),
+(16, 'Ciprian Dobre', 3, 'CC'),
+(17, 'Nicolae Tapus', 3, 'CA'),
+(17, 'Razvan Rughinis', 3, 'CB'),
+(17, 'Razvan Rughinis', 3, 'CC'),
+(18, 'Adrian Petrescu', 3, 'CA'),
+(18, 'Decebal Popescu', 3, 'CB'),
+(18, 'Decebal Popescu', 3, 'CC'),
+(19, 'Lorina Negreanu', 3, 'CA'),
+(19, 'Lorina Negreanu', 3, 'CB'),
+(19, 'Irina Mocanu', 3, 'CC'),
+(20, 'Florica Moldoveanu', 3, 'CA'),
+(20, 'Irina Mocanu', 3, 'CB'),
+(20, 'Marius Zaharia', 3, 'CC'),
+(21, 'Octavian Purdila', 3, 'CA'),
+(21, 'Marius Zaharia', 3, 'CB'),
+(21, 'Andrei Pitis', 3, 'CC'),
+(22, 'Nicolae Tapus', 3, 'CA'),
+(22, 'Emil Slusanschi', 3, 'CB'),
+(22, 'Emil Slusanschi', 3, 'CC'),
+(23, 'Nicolae Tapus', 3, 'CA'),
+(23, 'Nicolae Tapus', 3, 'CB'),
+(23, 'Nicolae Tapus', 3, 'CC'),
+(24, 'Mircea Petrescu', 3, 'CA'),
+(24, 'Florin Radulescu', 3, 'CB'),
+(24, 'Mircea Petrescu', 3, 'CC'),
+(25, 'Florica Moldoveanu', 3, 'CA'),
+(25, 'Florica Moldoveanu', 3, 'CB'),
+(25, 'Alin Moldoveanu', 3, 'CC'),
+(26, 'Costin Stefanescu', 3, 'CA'),
+(26, 'Costin Stefanescu', 3, 'CB'),
+(26, 'Costin Stefanescu', 3, 'CC');
--- branches/db/mysql-cspay-ore-fill.sql
+++ branches/db/mysql-cspay-ore-fill.sql
@@ -2,5 +2,27 @@
 -- Dumping data for table `ore`
 -- 
 
-INSERT INTO TABLE `ore` VALUES
-(1, 1, C, 'L', 'R', 1, 'CA', 145, 5, 'sg', 2, 2, 66, '3s', 'Gogu', 'B', 'Rughinis Razvan', 'Rughinis Razvan', '1CA', 'lu', '14-16', 'EC004', 1, 1);
+INSERT INTO `ore`(`link_disc`, `tip_ora`, `forma`, `cod`, `an`, `serie`, `nr_stud`, `nr_grupa`, `tip_grupa_aplicatii`, `nr_ore_curs`, `nr_ore_aplicatii`, `nr_post`, `grad_post`, `pers_norma`, `tip_ocupare`, `pers_acoperit`, `pers_acoperit_efect`, `an_grupa`, `zi`, `ora`, `sala`, `paritate`, `paritate_start`) VALUES
+(1, 'C', 'L', 'R', 1, 'CA', 145, 5, 'sg', 2, 2, 66, '3s', 'Rughinis Razvan', 'B', 'Rughinis Razvan', 'Rughinis Razvan', '3CA', 'lu', '14-16', 'EC004', 1, 1),
+(21, 'C', 'L', 'R', 3, 'CA', 145, 5, 'sg', 3, 2, 66, '3s', 'Gogu', 'po', 'Octavian Purdila', 'Octavian Purdila', '3CA', 'mi', '11-14', 'EC101', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 110, '4a', 'Razvan Deaconescu', 'B', 'Razvan Deaconescu', 'Razvan Deaconescu', '332aCA', 'vi', '8-10', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 110, '4a', 'Razvan Deaconescu', 'B', 'Razvan Deaconescu', 'Razvan Deaconescu', '334bCA', 'vi', '10-12', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Mihai Capota', 'Alex Mihaiuc', '331aCA', 'mi', '20-22', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Mihai Capota', 'Muraru Mihnea', '331bCA', 'jo', '14-16', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Mihai Capota', 'Daniel Baluta', '332bCA', 'vi', '14-16', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Mihai Capota', 'Lucian Grijincu', '333aCA', 'vi', '16-18', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Mihai Capota', 'Lucian Grijincu', '333bCA', 'vi', '18-20', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Iulian Moraru', 'Alexandru Mosoi', '334aCA', 'jo', '20-22', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Iulian Moraru', 'Alexandru Mihaiuc', '335aCA', 'mi', '18-20', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CA', 145, 5, 'sg', 2, 2, 116, '4a', 'Gogu', 'po', 'Iulian Moraru', 'Daniel Baluta', '335aCA', 'vi', '12-14', 'EG207', 1, 1),
+(21, 'C', 'L', 'R', 3, 'CB', 145, 5, 'sg', 3, 2, 62, '3s', 'Gogu', 'po', 'Mihai Capota', 'Marius Zaharia', '3CB', 'lu', '17-20', 'EC101', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 131, '4a', 'Mihai Capota', 'B', 'Mihai Capota', 'Mihai Capota', '335CBb', 'lu', '14-16', 'EC101', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 131, '4a', 'Mihai Capota', 'B', 'Mihai Capota', 'Mihai Capota', '333CBa', 'ma', '16-18', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Mihaela Dumitrache', 'Mihaela Dumitrache', '334CBa', 'lu', '8-10', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Mihaela Dumitrache', 'Mihaela Dumitrache', '332CBa', 'mi', '8-10', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Mihaela Dumitrache', 'Alexandru Egner', '331CBa', 'lu', '10-12', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Mihaela Dumitrache', 'Alexandru Egner', '331CBb', 'lu', '12-14', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Iulian Moraru', 'Marius Zaharia', '334CBa', 'mi', '10-12', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Iulian Moraru', 'Marius Zaharia', '332CBa', 'mi', '12-14', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Iulian Moraru', 'Marius Zaharia', '3325Ba', 'ma', '12-14', 'EG207', 1, 1),
+(21, 'L', 'L', 'R', 3, 'CB', 145, 5, 'sg', 2, 2, 135, '4a', 'Gogu', 'po', 'Marius Zaharia', 'Marius Zaharia', '3335Bb', 'ma', '18-20', 'EG207', 1, 1);
