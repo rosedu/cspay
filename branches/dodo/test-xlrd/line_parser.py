786fa634d7f0345d96df7ca9b57cf143e8fa6dfe
Grigore Lupescu <noemail@rosedu.org>
2009-07-16 17:20:59 +0000

    update importer dodo

--- branches/dodo/test-xlrd/line_parser.py
+++ branches/dodo/test-xlrd/line_parser.py
@@ -9,15 +9,69 @@
 #If the data is inconsistent it gives an error and halts execution
 
 def parse(list_data,line_number):
-    if not list_data:
-        return str(line_number)
+    #all cells in a line are empty=>ignore the line, return Ok
+    ii=0
+    for empty in list_data:
+        if not empty:
+            ii=ii+1
+        elif type(empty)==type('string'):
+            empty=""
+        elif type(empty)==type('int'):
+            empty=0
+            
+    if ii>21:
+        return "Ok"
+    
+    #common mandatory data 1(C+L, index 0->6)
+    i=0
+    while i<7:
+        if not list_data[i]:
+            print "Cell ",i,
+            return line_number 
+        i=i+1
+
+    #common mandatory data 2(C+L, index 12->13)
+    i=12
+    while i<14:
+        if not list_data[i]:
+            print "Cell ",i,
+            return line_number
+        i=i+1
+    #common mandatory data 3(C+L, index 15->21)
+    i=15
+    while i<22:
+        if not list_data[i]:
+            print "Cell ",i,
+            return line_number
+        i=i+1
+    
+
+    #mandatory data only for C
+    if list_data[1]=="C":
+        if not list_data[7]:
+            print "Cell ",7,
+            return line_number
+        if not list_data[8]:
+            print "Cell ",8,
+            return line_number
+        if not list_data[10]:
+            print "Cell ",10,
+            return line_number
 
     
+    #mandatory data only for L
+        #mandatory data only for C
+    elif list_data[1]=="L" or list_data[1]=="P":
+        if not list_data[9]:
+            print "Cell ",9,
+            return line_number
+        if not list_data[11]:
+            print "Cell ",11,
+            return line_number
+    else:
+        return line_number
+        #remove spacesa
     for value in list_data:
-        if not value:
-            return str(line_number)
-
-        #remove spaces
         if type(value)==type('string'): #check data type
             value.strip() #removes leading and trailing spaces from string types
         #to remove only leading (lstrip) and only trailing (rstrip)
--- branches/dodo/test-xlrd/script.py
+++ branches/dodo/test-xlrd/script.py
@@ -2,7 +2,7 @@
 import xlrd
 import line_parser
 import line_reader
-
+import db_writer
 #pointer to the xls file
 
 ################################################
@@ -11,7 +11,7 @@ import line_reader
 #
 ################################################
 
-file_name="model_small.xls"
+file_name="model.xls"
 file_exists=0
 while file_exists==0:
     
@@ -24,7 +24,6 @@ while file_exists==0:
         file_name=raw_input("Enter the correct path to the xls file !!! ")
         file_exists=0
 
-index_sheet=0
 
 ################################################
 #
@@ -32,23 +31,24 @@ index_sheet=0
 #
 ################################################
 
-while index_sheet<file_xls.nsheets:
-    index_line=0
-    #get sheet and pass it to read line
+index_line=2
+#get sheet and pass it to read line
    
-    sheet = file_xls.sheet_by_index(index_sheet)
-    while index_line<sheet.nrows:
-        line=line_reader.read_line(sheet,index_line)
+sheet = file_xls.sheet_by_index(0)
+while index_line<sheet.nrows:
+    line=line_reader.read_line(sheet,index_line)
         
-        error_check=line_parser.parse(line,index_line+1) #if "OK" data is consistent
-        if error_check!="Ok":
-            print "Data missing on line :",
-            print error_check," Sheet :",sheet.name
-            print "Please correct XLS file"
-            #index_sheet=file_xls.nsheets
-            #break
-        index_line=index_line+1
-    index_sheet=index_sheet+1
+    error_check=line_parser.parse(line,index_line) #if "OK" data is consistent
+    if error_check!="Ok":
+        print "Data missing on line :",
+        print error_check+1," Sheet :",sheet.name
+        print "Please correct XLS file"
+        #index_sheet=file_xls.nsheets
+        #break
+    else :
+        db_writer.db_write_line(line)
+    index_line=index_line+1
+    
 #test read first line in xls file -> uncomment next line to test
 #print "Line 0 : ",reader.read_line(xls_file,0,0)
 
